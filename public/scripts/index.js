f10806928cabcd98df3e6a5e6ca597515eaecc03
MichaelOtienno <michealvenum@gmail.com>
2023-10-21 22:02:22 +0300

    search functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -23,7 +23,7 @@ function fetchAndDisplayProducts() {
                 <p class="description">${truncator(product.description)}</p>
                 <p class="price">Ksh ${product.price}</p>
                 <p class="category"> ${product.category}</p>
-                <button>Add to Cart</button>
+                <button class="updatecart">Add to Cart</button>
                 </a>
                `;
 
@@ -39,11 +39,99 @@ function fetchAndDisplayProducts() {
 }
 
 function ProductClick(product) {
-    localStorage.setItem('selectedProduct', JSON.stringify(product));
+    localStorage.setItem('product', JSON.stringify(product));
     window.location.href = "/views/products.html";
 }
 
+//Category
+document.getElementById('category').addEventListener('change', filterProducts);
+
+function filterProducts() {
+    const selectedCategory = document.getElementById('category').value;
+    const productListDiv = document.querySelector('.Products');
+
+    let apiUrl = 'https://fakestoreapi.com/products';
+    if (selectedCategory) {
+        apiUrl = `https://fakestoreapi.com/products/category/${selectedCategory}`;
+    }
+
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(products => {
+            // Clear existing products
+            productListDiv.innerHTML = '';
+
+            products.forEach(product => {
+                const productsDiv = document.createElement('div');
+                productsDiv.classList.add('products-container');
+                productsDiv.innerHTML = `
+                <a href="products.html">
+                <h2>${product.title}</h2>
+                <img src="${product.image}" alt="${product.title}">
+                <p class="description">${truncator(product.description)}</p>
+                <p class="price">Ksh ${product.price}</p>
+                <p class="category"> ${product.category}</p>
+                <button class="updatecart">Add to Cart</button>
+                </a>
+               `;
+
+                productListDiv.appendChild(productsDiv);
+
+                const productLink = productsDiv.querySelector('a');
+                productLink.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    ProductClick(product);
+                });
+            });
+        });
+}
+
 window.onload = fetchAndDisplayProducts;
+
+//search
+document.getElementById('searchInput').addEventListener('input', performSearch);
+function performSearch() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const productListDiv = document.querySelector('.Products');
+
+    fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(products => {
+            const filteredProducts = products.filter(product => {
+                return product.title.toLowerCase().includes(searchInput) ||
+                    product.description.toLowerCase().includes(searchInput);
+            });
+
+            productListDiv.innerHTML = '';
+
+            filteredProducts.forEach(product => {
+                const productsDiv = document.createElement('div');
+                productsDiv.classList.add('products-container');
+                productsDiv.innerHTML = `
+                <a href="products.html">
+                <h2>${product.title}</h2>
+                <img src="${product.image}" alt="${product.title}">
+                <p class="description">${truncator(product.description)}</p>
+                <p class="price">Ksh ${product.price}</p>
+                <p class="category"> ${product.category}</p>
+                <button class="updatecart">Add to Cart</button>
+                </a>
+               `;
+
+                productListDiv.appendChild(productsDiv);
+
+                const productLink = productsDiv.querySelector('a');
+                productLink.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    ProductClick(product);
+                });
+            });
+        });
+}
+
+
+
+
 
 
 
--- public/scripts/products.js
+++ public/scripts/products.js
@@ -1,5 +1,7 @@
-if (localStorage.getItem('selectedProduct')) {
-    const selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
+//Products by Id
+
+if (localStorage.getItem('product')) {
+    const selectedProduct = JSON.parse(localStorage.getItem('product'));
     const productId = selectedProduct.id;
     const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
@@ -25,7 +27,7 @@ if (localStorage.getItem('selectedProduct')) {
                 <h2>${product.title}</h2>
                 <h4>${product.description}</h4>
                 <p>$${product.price.toFixed(2)}</p>
-                <button>Add to Cart</button>
+                <button id="btn">Add to Cart</button>
             `;
 
             productDiv.appendChild(image);
@@ -38,6 +40,91 @@ if (localStorage.getItem('selectedProduct')) {
 } else {
     console.log('No product selected in localStorage.');
 }
+
+
+
+function truncator(description) {
+    const words = description.split(' ');
+    if (words.length > 2) {
+        const truncatedDescription = words.slice(0, 7).join(' ') + '...';
+        return truncatedDescription;
+    }
+    return description;
+}
+
+function clickcart(){
+    document.getElementById('btn').addEventListener('click',(e)=>{
+        alert('almost there')
+    })
+}
+clickcart();
+
+
+//All products
+// function fetchAndDisplayProducts() {
+//     fetch('https://fakestoreapi.com/products')
+//         .then(res => res.json())
+//         .then(products => {
+//             const productListDiv = document.querySelector('.Products');
+//             products.forEach(product => {
+//                 const productsDiv = document.createElement('div');
+//                 productsDiv.classList.add('products-container');
+//                 productsDiv.innerHTML = `
+//                 <a href="products.html">
+//                 <h2>${product.title}</h2>
+//                 <img src="${product.image}" alt="${product.title}">
+               
+//                 <p class="description">${truncator(product.description)}</p>
+//                 <p class="price">Ksh ${product.price}</p>
+//                 <p class="category"> ${product.category}</p>
+//                 <button class= "cartBtn">Add to Cart</button>
+//                 </a>
+//                `;
+
+//                 productListDiv.appendChild(productsDiv);
+
+//                 const productLink = productsDiv.querySelector('a');
+//                 productLink.addEventListener('click', (e) => {
+//                     e.preventDefault();
+//                     ProductClick(product);
+//                 });
+//             });
+//         });
+// }
+
+// function ProductClick(product) {
+//     localStorage.setItem('product', JSON.stringify(product));
+//     window.location.href = "/views/products.html";
+// }
+
+// window.onload = fetchAndDisplayProducts;
+
+// var cartCountElement = document.getElementById('cart-count');
+// // Get the button element you want to attach the click event to
+// var button = document.getElementById('btn'); // Replace 'your-button-id' with the actual button ID or selector
+
+// // Add a click event listener to the button
+// button.addEventListener('click', function() {
+//     // Get the current cart count
+//     var currentCount = parseInt(cartCountElement.textContent);
+
+//     // Update the cart count
+//     var newCount = currentCount + 1;
+
+//     // Update the cart count element
+//     cartCountElement.textContent = newCount;
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
